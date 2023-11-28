f7ad158165e32e64d5c705dd92d05d53ffefb913
Ptolemy Morris <ptolemymorris@MacBook-Pro-von-Ptolemy.local>
2023-11-28 17:00:15 +0000

    Started Work on a UserSelector/Creator still not done but getting there

--- Profiles.py
+++ Profiles.py
@@ -1,62 +1,162 @@
 import yaml
 from pathlib import Path
-from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
+from PyQt5.QtWidgets import QApplication, QPushButton,QDialog,QFormLayout,QHBoxLayout,QCompleter,QLineEdit,QSlider,QLabel
+from PyQt5.QtCore import  pyqtSignal, QObject,QRect
+from PyQt5.QtGui import QFontDatabase,QDoubleValidator
 import sys
+import os
+
 ''' Profiles.yml
-Current: currentUserName
+Current: currentUsername
 Users: dictionary key = username, value = path/to/user
+DefaultPath: default path new users will be saved to
 '''
 def generateProfilesFile():
-    pass
+    data = {
+    'Current': None,
+    'Users': {},
+    'DefaultPath': None,
+    }
+    profilesPath = str(Path(__file__).resolve().parent) + "/Profiles.yml"
+    defaultPath = str(Path(__file__).resolve().parent) + "/Users"
+    if not os.path.exists(defaultPath):
+        os.mkdir(defaultPath)
+    data['DefaultPath'] = defaultPath
+    with open(profilesPath, 'w') as file:
+        yaml.dump(data, file, default_flow_style=False)
 
-def userFileGenerator(path):
-    pass
-class UserSelector(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setGeometry(100,100,300,130)
-        self.setWindowTitle("User Selection")
-        self.selectUserButton = QPushButton("Select User",self)
-        self.createUserButton = QPushButton("New User",self)
-        self.deleteUserButton = QPushButton("Delete User",self)
-        self.updateGeometry()
-        
-    def resizeEvent(self,event):
-        super().resizeEvent(event)
-        self.updateGeometry()
-    
-    def updateGeometry(self):
-        self.selectUserButton.setGeometry(int(self.width() * 1/6),int(self.height() * 2/13),int(self.width() * 2 / 3),int(self.height()* 4/13))
-        self.createUserButton.setGeometry(int(self.width() * 7.75/15),int(self.height() * 7/13),int(self.width() * 2/5),int(self.height() * 4/13))
-        self.deleteUserButton.setGeometry(int(self.width() * 1.25/15),int(self.height() * 7/13),int(self.width() * 2/5),int(self.height() * 4/13))
-        self.update()
-    
-    def selectUser(self):
-        pass
-    
-    def createUser(self):
-        pass
-    
-    def deleteUser(self):
-        pass
-    
-def getUserPreferences():
-    profilesPath = Path.cwd() + "/Profiles.yml"
+def getUserProfiles():
+    profilesPath = str(Path(__file__).resolve().parent) + "/Profiles.yml"
+    if not os.path.exists(profilesPath):
+        generateProfilesFile()
     try:
         with open(profilesPath, "r") as profilesFile:
             profiles = yaml.safe_load(profilesFile)
     except (FileNotFoundError, yaml.YAMLError):
         generateProfilesFile()
-    if profiles['Current'] is None:
-        user = UserSelector(profiles)    
-    user = profiles[profiles['Current']]
-    with open(user,"wr") as userPreferences:
-        userPreferences = yaml.safe_load(userPreferences)
-        # add something to update usersPath to current dir
-        return userPreferences
+    with open(profilesPath, "r") as profilesFile:
+            profiles = yaml.safe_load(profilesFile)
+    return profiles
+
+
+class Communicate(QObject):
+    closed = pyqtSignal(str) 
+class UserSelector(QDialog):
+    def __init__(self,userProfiles):
+        super().__init__()
+        self.setGeometry(100,100,400,200)
+        self.setWindowTitle("User Selection")
+        self.buttonInit()
+        self.initLayout()
+        self.communicate = Communicate()
+        self.userProfiles = userProfiles
+        self.currentUserSettings = {}
+        
+    def resizeEvent(self,event):
+        super().resizeEvent(event)
+
+    def initLayout(self):
+        
+        self.layout = QFormLayout()
+        self.setLayout(self.layout)
+        #adding fontrgba slider
+        fontrgbSlidersBox = QHBoxLayout()
+        self.fontrgbSliders  = {} #(r,g,b) sliders
+        self.fontrgbIndicators = {}
+        for colour in ("red","green","blue"):
+            slider, indicator = self.generateSlider(0,255)
+            slider.valueChanged.connect(self.updateSliderIndicators)
+            self.fontrgbSliders[colour] = slider
+            self.fontrgbIndicators[colour] = indicator
+            fontrgbSlidersBox.addWidget(indicator,6)
+            fontrgbSlidersBox.addWidget(slider,10)
+            
+        self.layout.addRow("Colour:",fontrgbSlidersBox)
+        #adding font opacity slider
+        self.fontOpacitySlider, self.fontOpacityIndicator = self.generateSlider(0,100)
+        self.fontOpacityIndicator.setText(str(self.fontOpacitySlider.value() /100))
+        opacityLayout = QHBoxLayout()
+        opacityLayout.addWidget(self.fontOpacitySlider)
+        opacityLayout.addWidget(self.fontOpacityIndicator)
+        self.layout.addRow("Font Opacity: ",opacityLayout)
+        self.fontOpacitySlider.valueChanged.connect(self.updateSliderIndicators)
+        #Adding font selector
+        availableFonts = QFontDatabase().families()
+        fontCompleter = QCompleter(availableFonts)
+        fontCompleter.setCaseSensitivity(False)
+        self.fontInput = QLineEdit("")
+        self.fontInput.setCompleter(fontCompleter)
+        self.layout.addRow("Font:" ,self.fontInput)
+        #adding font size selector
+        fontSizeValidator = QDoubleValidator()
+        fontSizeValidator.setNotation(QDoubleValidator.StandardNotation) #to be fix must stop , accepted as well as . (europian way of writing float)
+        self.fontSizeInput = QLineEdit("")
+        self.fontSizeInput.setValidator(fontSizeValidator)
+        self.layout.addRow("Font Size:",self.fontSizeInput)
+        #adding done and delete button
+        buttonLayout  = QHBoxLayout()
+        buttonLayout.setGeometry(self.layout.geometry())
+        buttonLayout.addWidget(self.deleteUserButton,2)
+        buttonLayout.addStretch(6)
+        buttonLayout.addWidget(self.doneButton,2)
+        self.layout.addRow(buttonLayout)
+
+    def buttonInit(self):
+        '''Initializes the buttons and connects functions'''
+        self.doneButton = QPushButton("Done",self)
+        self.createUserButton = QPushButton("Create User",self)
+        self.deleteUserButton = QPushButton("Delete User",self)
+        self.createUserButton.clicked.connect(self.createUser)
+        self.deleteUserButton.clicked.connect(self.deleteUser)
+        self.doneButton.clicked.connect(self.doneButtonFunction)
+
+    def doneButtonFunction(self):
+        self.saveUserProfile()
+        self.close()
     
+    def saveUserProfile(self):
+        userFilePath = self.userProfiles['Users'][self.userProfiles['Current']]
+        css_string = "QLabel {\n"
+        #adding rgba font colour
+        css_string += "color: rgba(" + str(self.fontrgbSliders["red"].value())
+        css_string += "," + str(self.fontrgbSliders["green"].value()) 
+        css_string += "," + str(self.fontrgbSliders["blue"].value()) 
+        css_string += "," + str(self.fontOpacitySlider.value() /100) + ");\n"
+        css_string += "font-family: " + str(self.fontInput.text()) + ";\n"
+        css_string += "font-size: " + str(self.fontSizeInput.text()) + "px;\n"
+        css_string += "}"
+        with open(userFilePath, 'w') as userFile:
+            userFile.write(css_string)
+
+    def createUser(self):
+        path = self.userProfiles['DefaultPath'] + "/User.css"
+        self.userProfiles['Users']['DefaultUser'] = path
+        self.userProfiles['Current'] = 'DefaultUser'
+        
+    def deleteUser(self):
+        os.remove(self.userProfiles['Users'][self.userProfiles['Current']])
+        del self.userProfiles['Users'][self.userProfiles['Current']]
+        self.userProfiles['Default'] = None
+    def updateSliderIndicators(self):
+        self.fontOpacityIndicator.setText(str(self.fontOpacitySlider.value() /100))
+        for colour in self.fontrgbIndicators.keys():
+            self.fontrgbIndicators[colour].setText(str(self.fontrgbSliders[colour].value()))
+
+
+    def generateSlider(self,min: int, max: int):
+        slider = QSlider()
+        slider.setOrientation(1)
+        slider.setRange(min,max)
+        indicator = QLabel()
+        indicator.setText(str(slider.value()))
+        return slider, indicator
+        
+    def closeEvent(self,event):
+        with open(str(Path(__file__).resolve().parent) + "/Profiles.yml", 'w') as file:
+            yaml.dump(self.userProfiles, file)
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = UserSelector()
+    window = UserSelector(getUserProfiles())
     window.show()
     sys.exit(app.exec_())
--- SubtitleWindow.py
+++ SubtitleWindow.py
@@ -2,13 +2,26 @@ import sys
 from PyQt5.QtWidgets import QApplication, QLabel, QMenu, QAction, QMainWindow
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFontMetrics
-
+from pathlib import Path
+import Profiles
+import yaml
+import os
 
 class SubtitleWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        #recieves userProfiles if unsuccessful then quits the application
+        self.profiles = Profiles.getUserProfiles()
+        if self.profiles['Current'] is None:
+            self.userSelector = Profiles.UserSelector(self.profiles)
+            self.userSelector.exec_()
+        self.profiles = Profiles.getUserProfiles()
+        if self.profiles['Current'] is None:
+            sys.exit()
+        else:
+            with open(self.profiles['Users'][self.profiles['Current']],"r") as userFile: #Opens Current User File I know its cursed plz somebody fix
+                self.css = userFile.read()
         # Set window properties
-        
         self.setWindowFlags(Qt.FramelessWindowHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
         self.setWindowFlag(2) # allows resizing
@@ -19,10 +32,9 @@ class SubtitleWindow(QMainWindow):
         self.dragging = False
         self.oldPosition = None
         self.translucentMode = True
-        # set label which contains the subtitlese
+        # set label which contains the subtitles
         self.label = QLabel(self)
-        self.label.setText("[Subtitles]")
-        self.label.setStyleSheet("font-size: 40px; color: Pink;")
+        self.label.setStyleSheet(self.css)
         self.label.setGeometry(0, 0, self.width(), self.height())
 
     #MouseEvents implement window movement without having to have a window with a frame 
@@ -45,11 +57,16 @@ class SubtitleWindow(QMainWindow):
             self.oldPosition = None
 
     def resizeEvent(self, event):
+        '''Resizes the window ensure the label with the text stays the same'''
         super().resizeEvent(event)
         self.label.setGeometry(0, 0, self.width(), self.height())
         self.update()
     
     def contextMenuEvent(self, event):
+        '''
+        Create the context menu that allows user to switch between Transparency mode
+        
+        '''
         contextMenu = QMenu(self)
         #transpancy
         translucent = QAction("",self)
@@ -67,6 +84,7 @@ class SubtitleWindow(QMainWindow):
             self.close()
             
     def switchTransparencyMode(self):
+        '''Transparency mode works on windows not make not sure why'''
         self.translucentMode = not self.translucentMode
         self.setAttribute(Qt.WA_TranslucentBackground,self.translucentMode)
         self.update()
@@ -76,21 +94,26 @@ class SubtitleWindow(QMainWindow):
         self.label.setGeometry(0,0,self.width(),self.height())
         self.update()
         
-    # updates subtitles requires updating to accomdate multiple lines of text
     def setSubtitleText(self, text):
+        '''
+        updates subtitles ensure they do not run of the screen\n
+        needs updating to accomdate multiple lines of text
+        '''
         if self.label.text() == "[Subtitles]":
             pass
         else:
             text = self.label.text() + " " + text
         size = QFontMetrics(self.label.font())
         text_split = text.split(" ")
-        while size.width(text) > self.width() and len(text_split) > 1:
+        while size.width(text) > self.width() and len(text_split) > 1: #removes the oldest word until it fits
             text_split = text.split(" ")
             text_split = text_split[1:]
             text = " ".join(text_split)
         self.label.setText(text)
         self.update()
+    
 
+    
 
 
 if __name__ == '__main__':
