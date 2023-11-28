c60be987ee7eb602715850319c1b033b24e4a30c
Ptolemy Morris <ptolemymorris@MacBook-Pro-von-Ptolemy.local>
2023-11-28 17:36:18 +0000

    Cleaned up the code a bit to improve readablity still not good enough though

--- Profiles.py
+++ Profiles.py
@@ -1,6 +1,6 @@
 import yaml
 from pathlib import Path
-from PyQt5.QtWidgets import QApplication, QPushButton,QDialog,QFormLayout,QHBoxLayout,QCompleter,QLineEdit,QSlider,QLabel
+from PyQt5.QtWidgets import QApplication, QPushButton,QDialog,QFormLayout,QHBoxLayout,QCompleter,QLineEdit,QSlider,QLabel,QComboBox
 from PyQt5.QtCore import  pyqtSignal, QObject,QRect
 from PyQt5.QtGui import QFontDatabase,QDoubleValidator
 import sys
@@ -38,48 +38,28 @@ def getUserProfiles():
             profiles = yaml.safe_load(profilesFile)
     return profiles
 
-
 class Communicate(QObject):
     closed = pyqtSignal(str) 
+
 class UserSelector(QDialog):
+
     def __init__(self,userProfiles):
         super().__init__()
         self.setGeometry(100,100,400,200)
         self.setWindowTitle("User Selection")
+        self.userProfiles = userProfiles
+        self.currentUserSettings = {}
         self.buttonInit()
         self.initLayout()
         self.communicate = Communicate()
-        self.userProfiles = userProfiles
-        self.currentUserSettings = {}
         
-    def resizeEvent(self,event):
-        super().resizeEvent(event)
+    ################ Initialization ################
 
     def initLayout(self):
-        
         self.layout = QFormLayout()
         self.setLayout(self.layout)
-        #adding fontrgba slider
-        fontrgbSlidersBox = QHBoxLayout()
-        self.fontrgbSliders  = {} #(r,g,b) sliders
-        self.fontrgbIndicators = {}
-        for colour in ("red","green","blue"):
-            slider, indicator = self.generateSlider(0,255)
-            slider.valueChanged.connect(self.updateSliderIndicators)
-            self.fontrgbSliders[colour] = slider
-            self.fontrgbIndicators[colour] = indicator
-            fontrgbSlidersBox.addWidget(indicator,6)
-            fontrgbSlidersBox.addWidget(slider,10)
-            
-        self.layout.addRow("Colour:",fontrgbSlidersBox)
-        #adding font opacity slider
-        self.fontOpacitySlider, self.fontOpacityIndicator = self.generateSlider(0,100)
-        self.fontOpacityIndicator.setText(str(self.fontOpacitySlider.value() /100))
-        opacityLayout = QHBoxLayout()
-        opacityLayout.addWidget(self.fontOpacitySlider)
-        opacityLayout.addWidget(self.fontOpacityIndicator)
-        self.layout.addRow("Font Opacity: ",opacityLayout)
-        self.fontOpacitySlider.valueChanged.connect(self.updateSliderIndicators)
+        self.initUserDropdownMenu()
+        self.initFontrgba()
         #Adding font selector
         availableFonts = QFontDatabase().families()
         fontCompleter = QCompleter(availableFonts)
@@ -101,12 +81,67 @@ class UserSelector(QDialog):
         buttonLayout.addWidget(self.doneButton,2)
         self.layout.addRow(buttonLayout)
 
+    def initUserDropdownMenu(self):
+        self.DropDownMenu = QComboBox()
+        for user in self.userProfiles['Users'].keys():
+            self.DropDownMenu.addItem(user)
+        self.DropDownMenu.addItem("Create New User")
+        self.DropDownMenu.currentIndexChanged.connect(self.changeCurrentUser)
+        self.layout.addWidget(self.DropDownMenu)
+
+    def changeCurrentUser(self):
+        if self.DropDownMenu.currentIndex + 1 == self.DropDownMenu.count():
+            pass
+        
+    ################ Deals with font colour ################ 
+
+    def initFontrgba(self):
+         #adding fontrgba slider
+        fontrgbSlidersBox = QHBoxLayout()
+        self.fontrgbSliders  = {} #(r,g,b) sliders
+        self.fontrgbIndicators = {}
+        for colour in ("red","green","blue"):
+            slider, indicator = self.generateSlider(0,255)
+            slider.valueChanged.connect(self.updateSliderIndicators)
+            self.fontrgbSliders[colour] = slider
+            self.fontrgbIndicators[colour] = indicator
+            fontrgbSlidersBox.addWidget(indicator,6)
+            fontrgbSlidersBox.addWidget(slider,10)
+        self.layout.addRow("Colour:",fontrgbSlidersBox)
+        #adding font opacity slider
+        self.fontOpacitySlider, self.fontOpacityIndicator = self.generateSlider(0,100)
+        self.fontOpacityIndicator.setText(str(self.fontOpacitySlider.value() /100))
+        opacityLayout = QHBoxLayout()
+        opacityLayout.addWidget(self.fontOpacitySlider)
+        opacityLayout.addWidget(self.fontOpacityIndicator)
+        self.layout.addRow("Font Opacity: ",opacityLayout)
+        self.fontOpacitySlider.valueChanged.connect(self.updateSliderIndicators)
+        self.currentUserSettings['color'] = "rgba(0,0,0)"
+
+    def generateSlider(self,min: int, max: int):
+        slider = QSlider()
+        slider.setOrientation(1)
+        slider.setRange(min,max)
+        indicator = QLabel()
+        indicator.setText(str(slider.value()))
+        return slider, indicator
+    
+    def updateSliderIndicators(self):
+        self.fontOpacityIndicator.setText(str(self.fontOpacitySlider.value() /100))
+        for colour in self.fontrgbIndicators.keys():
+            self.fontrgbIndicators[colour].setText(str(self.fontrgbSliders[colour].value()))
+        rgba  =  "rgba(" + str(self.fontrgbSliders["red"].value())
+        rgba += "," + str(self.fontrgbSliders["green"].value()) 
+        rgba += "," + str(self.fontrgbSliders["blue"].value()) 
+        rgba += "," + str(self.fontOpacitySlider.value() /100) + ");\n"
+        self.currentUserSettings['color']  = rgba
+    
+    ################ Deals with buttons ################
+
     def buttonInit(self):
         '''Initializes the buttons and connects functions'''
         self.doneButton = QPushButton("Done",self)
-        self.createUserButton = QPushButton("Create User",self)
         self.deleteUserButton = QPushButton("Delete User",self)
-        self.createUserButton.clicked.connect(self.createUser)
         self.deleteUserButton.clicked.connect(self.deleteUser)
         self.doneButton.clicked.connect(self.doneButtonFunction)
 
@@ -127,21 +162,13 @@ class UserSelector(QDialog):
         css_string += "}"
         with open(userFilePath, 'w') as userFile:
             userFile.write(css_string)
-
-    def createUser(self):
-        path = self.userProfiles['DefaultPath'] + "/User.css"
-        self.userProfiles['Users']['DefaultUser'] = path
-        self.userProfiles['Current'] = 'DefaultUser'
         
     def deleteUser(self):
         os.remove(self.userProfiles['Users'][self.userProfiles['Current']])
         del self.userProfiles['Users'][self.userProfiles['Current']]
         self.userProfiles['Default'] = None
-    def updateSliderIndicators(self):
-        self.fontOpacityIndicator.setText(str(self.fontOpacitySlider.value() /100))
-        for colour in self.fontrgbIndicators.keys():
-            self.fontrgbIndicators[colour].setText(str(self.fontrgbSliders[colour].value()))
 
+    ################ Utitily Functions ################
 
     def generateSlider(self,min: int, max: int):
         slider = QSlider()
@@ -150,10 +177,24 @@ class UserSelector(QDialog):
         indicator = QLabel()
         indicator.setText(str(slider.value()))
         return slider, indicator
-        
+
+    def emptyUserSettingDictionary(self):
+        userSettings = {}
+        userSettings['color'] = "rgba(0,0,0,0)"
+        userSettings['font-family'] = None
+        userSettings['font-size'] = None
+        return userSettings
+    
+    def getUserSettingsDictionary(self):
+        pass
+     ################ Window Wide Events ################
+
     def closeEvent(self,event):
         with open(str(Path(__file__).resolve().parent) + "/Profiles.yml", 'w') as file:
             yaml.dump(self.userProfiles, file)
+
+    def resizeEvent(self,event):
+        super().resizeEvent(event)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
