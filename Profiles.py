f7d8497d70780d75a9e7d7e63866f6389161e341
Ptolemy Morris <ptolemymorris@MacBook-Pro-von-Ptolemy.local>
2023-11-29 09:42:15 +0000

    To do before merge: Add updating of the dropdownmenu for deletion allow renaming of profiles / not just naviely saving over general fixes and thing still needs tiding

--- Profiles.py
+++ Profiles.py
@@ -48,8 +48,8 @@ class UserSelector(QDialog):
         self.setGeometry(100,100,400,200)
         self.setWindowTitle("User Selection")
         self.userProfiles = userProfiles
-        self.currentUserSettings = {}
-        self.buttonInit()
+        self.currentUserSettings = self.getUserSettingDictionary()
+        
         self.initLayout()
         self.communicate = Communicate()
         
@@ -60,38 +60,34 @@ class UserSelector(QDialog):
         self.setLayout(self.layout)
         self.initUserDropdownMenu()
         self.initFontrgba()
-        #Adding font selector
-        availableFonts = QFontDatabase().families()
-        fontCompleter = QCompleter(availableFonts)
-        fontCompleter.setCaseSensitivity(False)
-        self.fontInput = QLineEdit("")
-        self.fontInput.setCompleter(fontCompleter)
-        self.layout.addRow("Font:" ,self.fontInput)
-        #adding font size selector
-        fontSizeValidator = QDoubleValidator()
-        fontSizeValidator.setNotation(QDoubleValidator.StandardNotation) #to be fix must stop , accepted as well as . (europian way of writing float)
-        self.fontSizeInput = QLineEdit("")
-        self.fontSizeInput.setValidator(fontSizeValidator)
-        self.layout.addRow("Font Size:",self.fontSizeInput)
+        self.initFontSelector()
+        self.initFontSizeSelector()
+        self.buttonInit()
+        
         #adding done and delete button
-        buttonLayout  = QHBoxLayout()
-        buttonLayout.setGeometry(self.layout.geometry())
-        buttonLayout.addWidget(self.deleteUserButton,2)
-        buttonLayout.addStretch(6)
-        buttonLayout.addWidget(self.doneButton,2)
-        self.layout.addRow(buttonLayout)
+        
+
+    ################ Drop down menu ################
 
     def initUserDropdownMenu(self):
         self.DropDownMenu = QComboBox()
+        self.DropDownMenu.setEditable(True)
         for user in self.userProfiles['Users'].keys():
             self.DropDownMenu.addItem(user)
         self.DropDownMenu.addItem("Create New User")
-        self.DropDownMenu.currentIndexChanged.connect(self.changeCurrentUser)
+        print(self.DropDownMenu.count())
+        if self.DropDownMenu.count() == 1:
+            self.DropDownMenu.setCurrentText("")
+        self.DropDownMenu.activated.connect(self.changeCurrentUser)
         self.layout.addWidget(self.DropDownMenu)
+        
 
     def changeCurrentUser(self):
-        if self.DropDownMenu.currentIndex + 1 == self.DropDownMenu.count():
-            pass
+        for x in range(self.DropDownMenu.count() - 2,-1,-1):
+            if self.DropDownMenu.itemText(x) == "" or self.DropDownMenu.itemText(x) == "Create New User":
+                self.DropDownMenu.removeItem(x)
+        if self.DropDownMenu.currentIndex() + 1 == self.DropDownMenu.count():
+            self.DropDownMenu.setCurrentText("")
         
     ################ Deals with font colour ################ 
 
@@ -117,25 +113,48 @@ class UserSelector(QDialog):
         self.layout.addRow("Font Opacity: ",opacityLayout)
         self.fontOpacitySlider.valueChanged.connect(self.updateSliderIndicators)
         self.currentUserSettings['color'] = "rgba(0,0,0)"
-
-    def generateSlider(self,min: int, max: int):
-        slider = QSlider()
-        slider.setOrientation(1)
-        slider.setRange(min,max)
-        indicator = QLabel()
-        indicator.setText(str(slider.value()))
-        return slider, indicator
     
     def updateSliderIndicators(self):
         self.fontOpacityIndicator.setText(str(self.fontOpacitySlider.value() /100))
         for colour in self.fontrgbIndicators.keys():
             self.fontrgbIndicators[colour].setText(str(self.fontrgbSliders[colour].value()))
+        self.fontrgbaDictionary()
+
+    def fontrgbaDictionary(self):
         rgba  =  "rgba(" + str(self.fontrgbSliders["red"].value())
         rgba += "," + str(self.fontrgbSliders["green"].value()) 
         rgba += "," + str(self.fontrgbSliders["blue"].value()) 
-        rgba += "," + str(self.fontOpacitySlider.value() /100) + ");\n"
+        rgba += "," + str(self.fontOpacitySlider.value() /100) + ")"
         self.currentUserSettings['color']  = rgba
+
+    ################ Font Selector ################
+
+    def initFontSelector(self):
+        availableFonts = QFontDatabase().families()
+        fontCompleter = QCompleter(availableFonts)
+        fontCompleter.setCaseSensitivity(False)
+        self.fontSelector = QLineEdit("")
+        self.fontSelector.setCompleter(fontCompleter)
+        self.fontSelector.textChanged.connect(self.fontSelectorDictionary)
+        self.layout.addRow("Font:" ,self.fontSelector)
+
+    def fontSelectorDictionary(self):
+        if self.fontSelector.text() in QFontDatabase().families():
+            self.currentUserSettings['font-family'] = self.fontSelector.text()
     
+    ################ Font Size Selector ################
+    
+    def initFontSizeSelector(self):
+        fontSizeValidator = QDoubleValidator()
+        fontSizeValidator.setNotation(QDoubleValidator.StandardNotation) #to be fix must stop , accepted as well as . (europian way of writing float)
+        self.fontSizeSelector = QLineEdit("")
+        self.fontSizeSelector.setValidator(fontSizeValidator)
+        self.fontSizeSelector.textChanged.connect(self.fontSizeSelectorDictionary)
+        self.layout.addRow("Font Size:",self.fontSizeSelector)
+
+    def fontSizeSelectorDictionary(self):
+        self.currentUserSettings['font-size'] = self.fontSizeSelector.text() + "px"
+
     ################ Deals with buttons ################
 
     def buttonInit(self):
@@ -144,21 +163,29 @@ class UserSelector(QDialog):
         self.deleteUserButton = QPushButton("Delete User",self)
         self.deleteUserButton.clicked.connect(self.deleteUser)
         self.doneButton.clicked.connect(self.doneButtonFunction)
+        buttonLayout  = QHBoxLayout()
+        buttonLayout.setGeometry(self.layout.geometry())
+        buttonLayout.addWidget(self.deleteUserButton,2)
+        buttonLayout.addStretch(6)
+        buttonLayout.addWidget(self.doneButton,2)
+        self.layout.addRow(buttonLayout)
+
 
     def doneButtonFunction(self):
+        self.userProfiles['Current'] = self.DropDownMenu.currentText()
+        self.userProfiles['Users'][self.DropDownMenu.currentText()] = self.userProfiles['DefaultPath'] + "/" + self.DropDownMenu.currentText() + ".css"
         self.saveUserProfile()
         self.close()
     
     def saveUserProfile(self):
-        userFilePath = self.userProfiles['Users'][self.userProfiles['Current']]
+        if self.userProfiles['Current'] is None:
+            userFilePath = self.userProfiles['DefaultPath'] + "/" + self.DropDownMenu.currentText() + ".css"
+        else:
+            userFilePath = self.userProfiles['Users'][self.userProfiles['Current']]
         css_string = "QLabel {\n"
         #adding rgba font colour
-        css_string += "color: rgba(" + str(self.fontrgbSliders["red"].value())
-        css_string += "," + str(self.fontrgbSliders["green"].value()) 
-        css_string += "," + str(self.fontrgbSliders["blue"].value()) 
-        css_string += "," + str(self.fontOpacitySlider.value() /100) + ");\n"
-        css_string += "font-family: " + str(self.fontInput.text()) + ";\n"
-        css_string += "font-size: " + str(self.fontSizeInput.text()) + "px;\n"
+        for element in self.currentUserSettings.keys():
+            css_string += element + ": " + self.currentUserSettings[element] + ";\n"
         css_string += "}"
         with open(userFilePath, 'w') as userFile:
             userFile.write(css_string)
@@ -166,7 +193,7 @@ class UserSelector(QDialog):
     def deleteUser(self):
         os.remove(self.userProfiles['Users'][self.userProfiles['Current']])
         del self.userProfiles['Users'][self.userProfiles['Current']]
-        self.userProfiles['Default'] = None
+        self.userProfiles['Current'] = None
 
     ################ Utitily Functions ################
 
@@ -178,16 +205,18 @@ class UserSelector(QDialog):
         indicator.setText(str(slider.value()))
         return slider, indicator
 
-    def emptyUserSettingDictionary(self):
-        userSettings = {}
-        userSettings['color'] = "rgba(0,0,0,0)"
-        userSettings['font-family'] = None
-        userSettings['font-size'] = None
+    def getUserSettingDictionary(self):
+        if self.userProfiles['Current'] is None:
+            userSettings = {}
+            userSettings['color'] = "rgba(0,0,0,0)"
+            userSettings['font-family'] = None
+            userSettings['font-size'] = None
+        else:
+            userSettings = {}
+            # add code to generate dictionary from css file
         return userSettings
     
-    def getUserSettingsDictionary(self):
-        pass
-     ################ Window Wide Events ################
+    ################ Window Wide Events ################
 
     def closeEvent(self,event):
         with open(str(Path(__file__).resolve().parent) + "/Profiles.yml", 'w') as file:
