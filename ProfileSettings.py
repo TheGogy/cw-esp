c14372b3ac9af80f0db0af10b631c47e77354997
box1273002 <ptolemymorris@gmail.com>
2024-02-27 17:10:37 +0000

    Changes: 	Fixed minor indentation error coursed by code review

--- ProfileSettings.py
+++ ProfileSettings.py
@@ -270,9 +270,7 @@ class ProfileSettings(QFormLayout):
         return sliderBox
 
     def generate_slider_style(self, color):
-    '''
-    Returns a string containing the QSlider CSS
-    '''
+        '''Returns a string containing the QSlider CSS'''
         return f"""
             QSlider::groove:horizontal {{
                 background: qlineargradient(x1:0, y1:0, x2:1, y2:0,
