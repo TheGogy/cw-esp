d6e9a8a5765761d3eda68ea69aad4fe593b83721
George Rawlinson <thegogy@proton.me>
2024-04-26 16:18:07 +0100

    Fixed cleanup.sh for new version

--- cleanup.sh
+++ cleanup.sh
@@ -3,9 +3,9 @@
 if [ $# -ne 1 ]; then
 	echo "Usage: $0 <deep | shallow>"
 elif [ "$1" = "deep" ]; then
-	rm -rfv Users Models build dist Profiles.yml main.spec *__pycache__*
+	rm -rfv src/Users src/Models build dist Profiles.yml main.spec *__pycache__*
 elif [ "$1" = "shallow" ]; then
-	rm -rfv Users/*/ Models/*/ build dist Profiles.yml main.spec *__pycache__*
+	rm -rfv src/Users/*/ src/Models/*/ build dist Profiles.yml main.spec *__pycache__*
 else
 	echo "Usage: $0 <deep | shallow>"
 fi
